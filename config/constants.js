bde3ea1a1ddbd73afb8db65231991626f93334e2
Tim Morley <t_morley@argonet.co.uk>
2021-07-12 12:24:57 +0100

    added "bike shops" route

--- config/constants.js
+++ config/constants.js
@@ -6,6 +6,7 @@ export const SheetCodes = {
 	'South 1': '738737227',
 	'South 2': '1300962922',
 	Trumpington: '271355350',
+	'bike shops': '318778154',
 	// West: '855253552',
 	Demo: '1683354199',
 	DishOfTheDay: '169944562',
