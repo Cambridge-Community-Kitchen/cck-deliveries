87ccbc1192e02835d562259b82ad271082aaba66
timsk <t_morley@argonet.co.uk>
2021-11-27 15:13:53 +0000

    change 'return to base' from Lockon to Downing Place

--- components/back-to-lockon/BackToLockon.jsx
+++ components/back-to-lockon/BackToLockon.jsx
@@ -3,8 +3,8 @@ import { Box, Button } from '@chakra-ui/react';
 import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 const BackToLockon = () => {
-	const encodedGoogleMapsUrl = `https://goo.gl/maps/TKn359eKxHWskMJV6`;
-
+//	const encodedGoogleMapsUrl = `https://goo.gl/maps/TKn359eKxHWskMJV6`; // The Lockon
+        const encodedGoogleMapsUrl = `https://goo.gl/maps/yURo7vxnViPK3ngf7`; // Downing Place
 	return (
 		<Box d="flex" alignItems="center" justifyContent="center" py={5}>
 			<Button
@@ -15,7 +15,7 @@ const BackToLockon = () => {
 				size="sm"
 				target="_blank"
 			>
-				Back to The Lockon
+				Back to Downing Place
 			</Button>
 		</Box>
 	);
